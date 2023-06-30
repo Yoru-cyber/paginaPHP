77e6c41276175cc21da70eea5408f721d1115aa8
Yoru-cyber <carlosmendez170210@gmail.com>
2023-06-30 15:12:45 -0400

    pagina ya casi lista, funcionalidad por mejorar y diseño

--- Data/formHandler.php
+++ Data/formHandler.php
@@ -1,30 +1,36 @@
 <?php
-function convertTrueSpeed(){
-
-$userSpeed = $_POST["user_Download_Speed"];
-
+$userSpeed = $_POST["user_Download_Speed"]??null;
+$userDownloadType = $_POST["user_Download_Type"]??null;
+if ($userSpeed < 8 || $userDownloadType == "MB/s") {
+    $userDownloadType = "Kbps";
+}
+$userDownloadSize = $_POST["user_Download_Size"]??null;
+$userDownloadSizeFormat = $_POST["user_Download_Size_Format"]??null;
+switch ($userDownloadSizeFormat) {
+    case "GB":
+        $userDownloadSize = $userDownloadSize * 1000;
+        $userDownloadSizeFormat = "MB";
+        break;
+}
 $numberBits = 8;
 $trueSpeed = $userSpeed/$numberBits;
 
-return $trueSpeed;
-
-}
-function getDownloadFormat(){
-    $userDownloadType = $_POST["user_Download_Type"];
-
-    return $userDownloadType;
-    
-}
 function hacer_check()
 {
+    global $userDownloadType, $trueSpeed, $downloadTime, $userDownloadSize;
+    $downloadTime = $userDownloadSize/$trueSpeed;
+    
+   
     if (empty($_POST["user_Download_Speed"])&&empty($_POST["user_Download_Type"])) {
 
-        echo '<p>No has insertado tu velocidad :( </p>';
-        echo '<p>No has insertado tu formato de información :( </p>';
+        echo '<p>No has insertado tu velocidad ¯\_( ͡° ͜ʖ ͡°)_/¯ </p>';
+        echo '<p>No has insertado tu formato de información ¯\_( ͡° ͜ʖ ͡°)_/¯ </p>';
     }
     else{
-        echo '<p>Su velocidad real es: ' . convertTrueSpeed() . getDownloadFormat(). '</p';
-        
+        echo '<p>Su velocidad real es: ' . $trueSpeed .' '. $userDownloadType. '</p>';
+        echo '<p>Su tiempo estimado de descarga es: ' . $downloadTime . ' segundos</p>';
+        echo '<p>Su tiempo estimado de descarga es: ' . number_format(($downloadTime / 60), 4). ' minutos</p>';
+        echo '<p>Su tiempo estimado de descarga es: ' . number_format(($downloadTime/60)/60, 4) . ' horas</p>';
     }
 
 
--- index.php
+++ index.php
@@ -5,47 +5,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/main.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>Calculadora de Tiempo de Descarga</title>
 </head>
 <header>
     <center>
-        <div>
-            <h1>Calculadora de Tiempo de Descarga</h1>
+
+        <div style="width: 100%; height: auto; padding:0px;">
+            <img src="styles\img\punpunTriangle.png" style="width: 120px; height:80px; float: left; margin: 0px">
+            <h1 style="font-family: 'Monserrat', sans-serif; font-weight:100;">Calculadora de Tiempo de Descarga</h1>
+
 
         </div>
     </center>
 </header>
 
 <body>
-    <section id="calculator">
+    <section id="calculator" style="margin: 50px;">
         <center>
 
-            <div id="box">
-                <table>
-                    <form method="post">
+            <div id="box" >
+                
+
                     <?php
                     include 'Data/formHandler.php';
                     echo '<form method="post">
-
-                    Inserte su velocidad de internet: <input type="number" name="user_Download_Speed"><br><br>
-                    Inserte su unidad de información: <input list="speedList" placeholder="Ejemplo: KB/s" name="user_Download_Type">
-
+                    <p>
+                    Inserte su velocidad de internet: <input type="number" name="user_Download_Speed" required><br><br>
+                    Inserte su unidad de información: <input list="speedList" placeholder="Ejemplo: KB/s" name="user_Download_Type" required>
+                    <br><br>
+                    Inserte el tamaño de su descarga: <input type="number" name="user_Download_Size" required><input list="downloadList" placeholder="Ejemplo: GB" name="user_Download_Size_Format" required>
+                    <br>
+                    </p>
                     <datalist id="speedList">
                     <option value="KB/s">
-                    <option value="MB/S">
+                    <option value="MB/s">
                     <option value="GB/s">
                     </datalist> 
-                    <input type="submit">
+                    <datalist id="downloadList">
+                    <option value="KB">
+                    <option value="MB">
+                    <option value="GB">
+                    </datalist>
+                    <br>
+                    <input type="submit" value="Chuu">
 
                      </form>';
-                    
+
                     hacer_check();
-                    
+
+
+
+                    ?>
 
                 
-                    ?>
-                    </form>
-                </table>
+                <img id="punpun" src="styles\img\punpunwalking.gif">
             </div>
 
         </center>
